d34379fce6a8bf6724f5965004778b88b51c281d
Daria Gontar <dariall23ll@gmail.com>
2020-01-01 23:32:39 +0100

    RWD done except hamburger menu. work in progress

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
+// document.addEventListener('DOMContentLoaded', (event) => {
+    
+
+
 /*
 This code is about to change, to  be shorter. 
 Just something won't work with loop, so for now it has to be longer version.
  */
+
 const p=document.querySelector(".basic");
 function myF(){
     
     let k=document.querySelector("#btn1");
     k.style.top= "69px";
+    let w=window.innerWidth;
+    if (w<1000){
+        k.style.top= "-180px"
+    }
 }
 function myF2(){
     let k=document.querySelector("#btn1")
     k.style.top= "32px";
+    w=window.innerWidth;
+    if (w<1000){
+        k.style.top= "-180px"
+    }
 }
 p.addEventListener("mouseover", myF);
 p.addEventListener("mouseout", myF2);
@@ -20,10 +33,18 @@ function myF11(){
     
     let ki=document.querySelector("#btn2");
     ki.style.top= "69px";
+    let wi=window.innerWidth;
+    if (wi<1000){
+        ki.style.top= "-180px"
+    }
 }
 function myF22(){
     let ki=document.querySelector("#btn2")
     ki.style.top= "32px";
+    wi=window.innerWidth;
+    if (wi<1000){
+        ki.style.top= "-180px"
+    }
 }
 pr.addEventListener("mouseover", myF11);
 pr.addEventListener("mouseout", myF22);
@@ -33,10 +54,86 @@ function myF111(){
     
     let kid=document.querySelector("#btn3");
     kid.style.top= "69px";
+    let win=window.innerWidth;
+    if (win<1000){
+        kid.style.top= "-180px"
+    }
 }
 function myF222(){
     let kid=document.querySelector("#btn3")
     kid.style.top= "32px";
+    win=window.innerWidth;
+    if (win<1000){
+        kid.style.top= "-180px"
+    }
 }
 prem.addEventListener("mouseover", myF111);
 prem.addEventListener("mouseout", myF222);
+
+//calculator
+
+
+function productsQuantity(){
+    
+    const quantity=document.querySelector(".first_input");
+    const num_prods=document.querySelector(".number_of_products");
+    num_prods.innerHTML=quantity.value;
+}
+function ordersQuantity(){
+    
+    const orders=document.querySelector(".second_input");
+    const num_orders=document.querySelector(".number_of_orders");
+    num_orders.innerHTML=orders.value;
+}
+
+//showing my options then default ones
+    const select=document.querySelector(".select");
+
+    // select.addEventListener("click",function(){
+    //     const select2=document.querySelector(".select2");
+    //     select2.removeAttribute("hidden");
+    // });
+
+//
+const opt1=document.querySelector("#opt1");
+const pckg=document.querySelector(".choosen_package");
+        
+console.log(opt1.value);
+select.addEventListener("click", function(e){
+
+    
+    opt1.addEventListener("click",function(){
+        opt1.innerHTML="Basic"
+            pckg.innerHTML=opt1.innerHTML;
+        
+    })
+    
+})
+        
+//     opt1.addEventListener("click", function(){
+        
+//     })
+    
+
+// function tryMe(){
+//     if ()
+// }
+// function choseOption(){
+
+
+        
+// }
+
+
+
+
+const opt2=document.querySelector(".opt2");
+const opt3=document.querySelector(".opt3");
+
+
+
+
+
+
+
+// });
